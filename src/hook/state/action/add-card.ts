66b4649338e51987767d19b9c9afd75dc3246882
Tim Lonsdale <timlonsdale@gmail.com>
2026-06-21 21:59:19 +0100

    [FIX] Loaded cards get added to the current deck

--- src/hook/state/action/add-card.ts
+++ src/hook/state/action/add-card.ts
@@ -1,5 +1,6 @@
 import type * as card from "@src/card"
 import type * as hook from "@src/hook"
+import * as util from "@src/util"
 
 export type Action = {
   type: "ADD_CARD";
@@ -9,6 +10,7 @@ export type Action = {
 export function reduce(state: hook.state.State, action: Action): hook.state.State {
     return {
         ...state,
+        deck: util.random.shuffle([...state.deck, action.card]),
         source: [...state.source, action.card],
     }
 }
